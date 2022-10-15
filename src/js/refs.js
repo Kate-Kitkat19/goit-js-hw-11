19aeb304fa6cc8cf05409a44a1268c9f398f6028
Kate-Kitkat19 <katekateee1911@gmail.com>
2022-10-15 16:35:12 +0300

    222

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,5 @@
 const formRef = document.querySelector('form#search-form');
-const inputRef = document.querySelector('input[name="searchQuery"]');
 const container = document.querySelector('div.gallery');
 const loadMoreBtnRef = document.querySelector('button.load-more');
-const galleryLink = document.querySelector('a.gallery__item');
 
-export { formRef, inputRef, container, loadMoreBtnRef, galleryLink };
+export { formRef, container, loadMoreBtnRef };
--- src/js/renderMarkup.js
+++ src/js/renderMarkup.js
@@ -8,27 +8,25 @@ export function renderMarkup(picture) {
     comments,
     downloads,
   } = picture;
-  return `<a class="gallery__item" href="${largeImageURL}">
-  <div class="photo-card">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b>
-      ${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>
-      ${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>
-      ${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>
-      ${downloads}
-    </p>
-  </div>
-</div>
-</a>`;
+  return `<div class="photo-card">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+              ${likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b>
+              ${views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b>
+              ${comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b>
+              ${downloads}
+            </p>
+          </div>
+        </div>`;
 }
